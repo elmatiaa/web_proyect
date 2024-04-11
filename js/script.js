a2f15ee608be3435b5dfe112e039032226017003
Gabriela <ga.riverap@duocuc.cl>
2024-04-11 12:15:17 -0400

    actualizacion porfa funciona

---
+++ js/script.js
@@ -0,0 +1,18 @@
+if (document.getElementById('menu')) {
+    fetch('menu_superior.html').then(response => {
+        return response.text();
+    }).then(htmlContent => {
+        document.getElementById('menu').innerHTML = htmlContent;
+        window.scrollTo(0, 0);
+    });
+};
+
+//Duplica elementos
+if (document.getElementById('mini_producto')) {
+    var tarjeta = document.getElementById('mini_producto').outerHTML;
+    var tarjetas = '';
+    for (i = 0; i < 10; i++) {
+        tarjetas = tarjetas + tarjeta;
+    }
+    document.getElementById('mini_producto').outerHTML = tarjetas;
+}
