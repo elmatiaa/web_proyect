25b7c99b75ebea0bf35d9e3721a2e535b1aa3c93
Gabriela <ga.riverap@duocuc.cl>
2024-04-13 17:28:15 -0400

    actualizacion 13 de abril

--- js/script.js
+++ js/script.js
@@ -25,3 +25,24 @@ if (document.getElementById('pie')) {
         window.scrollTo(0, 0);
     });
 };
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
