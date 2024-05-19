b45370ee0f2957eec5c04ce58a4b91899164e5ed
elmatiaa <mat.moralesy@duocuc.cl>
2024-05-19 17:00:29 -0400

    actualizacion de validaciones (users, mis datos, ingresar)

--- js/validar.js
+++ js/validar.js
@@ -55,7 +55,7 @@ $("#formulario-registro").validate({
   rules: {
     rut: {
       required: true,
-      rutChileno: true
+      rutChileno: true,
     },
     nombres: {
       required: true,
@@ -118,4 +118,132 @@ $("#formulario-registro").validate({
     },
   },
 });
+});
+
+//validacion de login
+$(document).ready(function() {
+  //---CORREO ELECTRONICO
+  $.validator.addMethod("emailCompleto", function(value, element) {
+
+    // Expresión regular para validar correo electrónico
+    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z\-0-9]{2,}))$/;
+
+    // Validar correo electrónico con la expresión regular
+    return regex.test(value);
+
+  }, 'Ingrese un correo válido');
+
+  $("#formulario-ingreso").validate({
+    rules: {
+      cuenta: {
+        required: true,
+        emailCompleto: true,
+      },
+      contrasena: {
+        required: true,
+        minlength: 5,
+      }
+    },
+    messages: {
+      cuenta: {
+        required: "El email es un campo obligactorio",
+        email: "El email no cumple el formato de un correo",
+      },
+      contrasena: {
+        required: "La contraseña es una campo obligatorio",
+        minlength: "Contraseña no válida"
+      },
+    },
+  });
+});
+
+$(document).ready(function() {
+  //---CORREO ELECTRONICO
+  $.validator.addMethod("emailCompleto", function(value, element) {
+
+    // Expresión regular para validar correo electrónico
+    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z\-0-9]{2,}))$/;
+
+    // Validar correo electrónico con la expresión regular
+    return regex.test(value);
+
+  }, 'Ingrese un correo válido');
+
+  // -- VERIFICACION RUT
+  $.validator.addMethod("rutChileno", function(value, element) {
+    // Eliminar puntos y guión del RUT
+    value = value.replace(/[.-]/g, "");
+    // Validar que el RUT tenga 8 o 9 dígitos
+    if (value.length < 8 || value.length > 9) {
+      return false;
+    }
+    // Validar que el último dígito sea un número o una 'K'
+    var validChars = "0123456789K";
+    var lastChar = value.charAt(value.length - 1).toUpperCase();
+    if (validChars.indexOf(lastChar) == -1) {
+      return false;
+    }
+    // Calcular el dígito verificador
+    var rut = parseInt(value.slice(0, -1), 10);
+    var factor = 2;
+    var sum = 0;
+    var digit;
+    while (rut > 0) {
+      digit = rut % 10;
+      sum += digit * factor;
+      rut = Math.floor(rut / 10);
+      factor = factor === 7 ? 2 : factor + 1;
+    }
+    var dv = 11 - (sum % 11);
+    dv = dv === 11 ? "0" : dv === 10 ? "K" : dv.toString();
+
+    // Validar que el dígito verificador sea correcto
+    return dv === lastChar;
+  }, "Por favor ingrese un RUT válido."); 
+
+  $("#formulario-usuario").validate({
+    rules: {
+      identificador: {
+        required: true,
+      },
+      rut: {
+        required: true,
+        rutChileno: true,
+      },
+      nombres: {
+        required: true,
+        minlength: 3,
+      },
+      apellidos: {
+        required: true,
+        minlength: 3,
+      },
+      correo: {
+        required: true,
+        emailCompleto: true,
+      },
+      direccion: {
+        required: true,
+        minlength: 6,
+      },
+      contrasena: {
+        required: true,
+        minlength: 5,
+      },
+    },
+    messages: {
+      identificador: {
+        required: "El campo ID es obligatorio",
+      },
+      nombres: {
+        required: "El nombre es un campo obligatorio",
+        minlength: "Mínimo 3 caracteres",
+        soloLetras: "El nombre sólo puede contener letras y espacios en blanco",
+      },
+      apellidos: { 
+        required: "Apellidos es un campo obligatorio",
+        minlength: "Mínimo 3 caracteres",
+      },
+    }
+  });
 });
